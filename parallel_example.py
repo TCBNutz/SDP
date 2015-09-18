e7f352603c945eb52b962178d5a4ce5f8a5187ac
Pete Shadbolt <pete.shadbolt@gmail.com>
2015-09-18 14:36:49 +0100

    With cvxopt import

--- parallel_example.py
+++ parallel_example.py
@@ -8,8 +8,10 @@
 from numpy import *
 import multiprocessing
 from signal import signal, SIGINT, SIG_IGN
+from cvxopt import matrix, solvers, sparse
 import os
 import json
+import time
 
 CPUs = multiprocessing.cpu_count()
 IS_CLUSTER = CPUs > 4
@@ -18,14 +20,12 @@ DATA_DIR = "/work/pshadbol/fc2/" if IS_CLUSTER else "."
 
 def run(i):
     """ A slow process """
+    time.sleep(10)
     print i
-    return 0
 
 
 if __name__ == '__main__':
-    attempts = 50 if IS_CLUSTER else 10
+    attempts = 10 if IS_CLUSTER else 4
     pool = multiprocessing.Pool(CPUs, lambda: signal(SIGINT, SIG_IGN))
     wins = pool.map(run, range(attempts))
-
-
 
